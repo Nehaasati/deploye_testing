adb7ede34508ded584dd3119add338a1bf677d04
Nehaasati <31asatineha@gmail.com>
2026-04-17 04:14:05 +0200

    add test floder

--- server/server/obj/Debug/net10.0/server.AssemblyInfo.cs
+++ server/server/obj/Debug/net10.0/server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e86133277e760aa19e7ed6c010a39beb6cebd227")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e30de08ba9c486a33c2fd6cbe8f9f47aa430a86c")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
