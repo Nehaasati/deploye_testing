53a126616f10b5364f438ce9cfd0c813e4c1943f
Nehaasati <31asatineha@gmail.com>
2026-04-20 13:21:30 +0200

    added server.csproj

--- server/server/obj/Debug/net10.0/server.AssemblyInfo.cs
+++ server/server/obj/Debug/net10.0/server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+adb7ede34508ded584dd3119add338a1bf677d04")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73323981a908f3c2b9e8bd5aec815b433b65d9b3")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
